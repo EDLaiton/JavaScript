9d7fefa69efaf4ca64ba60ddeb0d4ba09819a4d1
EDLaiton <estebanlaiton98@gmail.com>
2023-08-30 21:18:25 -0500

    Condicionales

---
+++ condicionales.js
@@ -0,0 +1,10 @@
+const nombre = "Ada School";
+let edad = 123;
+let nota = 8;
+
+if(nombre === "Ada School" && nota >= 7) {
+    console.log("Hola " + nombre + " su nota es = " + nota);
+} else {
+    console.log("Nombre no existe");
+}
+
